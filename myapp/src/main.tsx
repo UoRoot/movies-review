cb3cca41b5d340f81a14284eeed537a29810a11e
burntpro <burnedprogrammer@gmail.com>
2024-09-11 18:49:10 -0500

    removing unnecessary files

--- myapp/src/main.tsx
+++ myapp/src/main.tsx
@@ -2,14 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.js";
 import { BrowserRouter } from "react-router-dom";
 import { SearchProvider } from "./context/search/search-context-provider.js";
-import { SearchMovieProvider } from "./context/search/search-movie-context-provider.js";
 
 createRoot(document.getElementById("root") as Element).render(
   <BrowserRouter>
     <SearchProvider>
-      <SearchMovieProvider>
-        <App />
-      </SearchMovieProvider>
+      <App />
     </SearchProvider>
   </BrowserRouter>
 );
